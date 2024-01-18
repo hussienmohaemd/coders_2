8ddd2bf081af7e8281405f52d1a047d9bc3ea7e2
hussien mohamed hesham <hussienmohamedone8@gmail.com>
2024-01-18 12:27:10 -0800

    Add files via upload

---
+++ o2.py
@@ -0,0 +1,12 @@
+import p1
+import random
+
+print(p1.x)
+
+import numpy 
+import pandas 
+import matplot
+
+x = [1,1,2,32,54,4,5]
+
+matplot.plot(x)
---
+++ p1.py
@@ -0,0 +1,12 @@
+x = 10
+def add(e,r):
+    return e+r
+
+def multi(e,r):
+    return e+r
+
+def add3(e,r):
+    return e+r
+
+def add4(e,r):
+    return e+r
